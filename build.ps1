ef809bf3b4c7fb56640b47ef1105ea867c277bb6
marginal23326 <58261815+marginal23326@users.noreply.github.com>
2026-06-14 21:51:29 +0600

    build: disable fmt debug info

--- build.ps1
+++ build.ps1
@@ -3,7 +3,7 @@ $Target = (rustc -Vv | Select-String "host:").Line.Split(" ")[1]
 cargo +nightly build --release `
   --target $Target `
   "-Zbuild-std=std,panic_abort" `
-  --config "target.$Target.rustflags='-Zunstable-options -Cpanic=immediate-abort'"
+  --config "target.$Target.rustflags='-Zunstable-options -Cpanic=immediate-abort -Zfmt-debug=none'"
 
 New-Item -ItemType Directory -Force -Path "./target/release" | Out-Null
 Copy-Item "./target/$Target/release/athan.exe" "./target/release/athan.exe" -Force
--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ TARGET=$(rustc -Vv | grep host | cut -d ' ' -f 2)
 cargo +nightly build --release \
   --target "$TARGET" \
   -Zbuild-std=std,panic_abort \
-  --config "target.$TARGET.rustflags='-Zunstable-options -Cpanic=immediate-abort'"
+  --config "target.$TARGET.rustflags='-Zunstable-options -Cpanic=immediate-abort -Zfmt-debug=none'"
 
 mkdir -p ./target/release
 cp "./target/$TARGET/release/athan" ./target/release/athan
